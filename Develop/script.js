d68e6d8b636c50164cb30757ebc0dc84312d4f26
rileysong01 <rsong47@uwo.ca>
2023-08-10 20:41:24 -0400

    firstprompt error

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,34 @@
-// Assignment Code
+// declare variables
+var lengthPerference = "";
+var pwLowercase = true;
+var pwUppercase = true;
+var pwNumeric = true;
+var pwSpecialChar = true;
+
+function firstPrompt() { 
+  do {
+  lengthPerference = window.prompt("How long do you want your password to be? (Please enter number between 8 and 128.)");
+  if (isNotWholeNumber(lengthPerference) || lengthPerference < 8 || lengthPerference > 128) {
+    alert("Please enter a valid numeric value between 8 and 128");
+  }
+  } while (isNaN(lengthPerference) && lengthPerference >=8 && lengthPerference <=128);
+} 
+
+
+
+
+// Selects the generate button!
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  //displas the password generated in the password box 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+//first prompt appears when generate button is clicked
+generateBtn.addEventListener("click", firstPrompt);
+
